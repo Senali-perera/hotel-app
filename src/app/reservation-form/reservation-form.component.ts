ddb98e26a8338f26c309b34a6d9ce0ab8500e533
Senali-perera <senaliperera1996@gmail.com>
2024-01-11 23:05:21 +0200

    Added HTML functions for reservation form component

--- src/app/reservation-form/reservation-form.component.ts
+++ src/app/reservation-form/reservation-form.component.ts
@@ -10,4 +10,8 @@ import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 export class ReservationFormComponent {
 
   reservationForm : FormGroup = new FormGroup({})
+
+  onSubmit(){
+
+  }
 }
