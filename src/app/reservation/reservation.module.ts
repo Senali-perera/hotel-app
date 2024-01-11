af5a9df408f8a11cfcdf258037ba049d7fd2c9ba
Senali-perera <senaliperera1996@gmail.com>
2024-01-11 22:56:00 +0200

    Created form group

--- src/app/reservation/reservation.module.ts
+++ src/app/reservation/reservation.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { ReservationFormComponent } from "../reservation-form/reservation-form.component";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ReservationFormComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class ReservationModule { }
